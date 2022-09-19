dc6008ca8c2c7cdf8118df05ba4304237afa8592
geraldkachi <fitzgeraldkachi@gmail.com>
2022-09-19 04:47:55 +0100

    latest on charts

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./main.css"
 import Cards from '../Cards/Cards'
 import Table from '../Table/Table'
 import tableData from '../../data/data.json'
 import ThreeDots from "../../assets/threedots.svg"
+import LineChart from '../lineChart.tsx/LineChart'
+
+
+
+export const UserData = [
+    {
+        id: 1,
+        day: "Mon",
+        adsWatched: 0,
+        userLost: 0,
+    },
+    {
+        id: 2,
+        day: "Tue",
+        adsWatched: 0,
+        userLost: 0,
+    },
+    {
+        id: 3,
+        day: "Wed",
+        adsWatched: 0,
+        userLost: 0,
+    },
+    {
+        id: 4,
+        day: "Thu",
+        adsWatched: 0,
+        userLost: 0,
+    },
+    {
+        id: 5,
+        day: "Fri",
+        adsWatched: 0,
+        userLost: 0,
+    },
+    {
+        id: 6,
+        day: "Sat",
+        adsWatched: 0,
+        userLost: 0,
+    },
+    {
+        id: 7,
+        day: "Sun",
+        adsWatched: 0,
+        userLost: 0,
+    },
+];
+
 
 const customerTableHead = [
     'Name Of Task',
@@ -16,6 +65,20 @@ const customerTableHead = [
 ]
 
 const Main = () => {
+
+
+    const [userData] = useState({
+        labels: UserData.map((data) => data.day.toUpperCase()),
+        datasets: [
+            {
+                label: "Ads Watched",
+                data: UserData.map((data) => data.adsWatched),
+                backgroundColor: "#2a71d0",
+                borderColor: "#2a71d0",
+                borderWidth: 2,
+            },
+        ],
+    })
 
     const renderHead = (item: any, index: number) => <th style={{ color: '#768396', fontSize: '14px' }} key={index}>{item}</th>
 
@@ -41,13 +104,16 @@ const Main = () => {
             </div>
 
 
+            <LineChart chartData={userData} />
+
+
             <div className='tablebody'>
 
                 <div className="taskseperate">
                     <h3>Task</h3>
 
-                    <select name="" id="" style={{padding:'10px', borderRadius:'10px', background:'#E8EDF1', borderColor:'#4BA8A8'}}>
-                        <option value="" style={{color:'#738383'}}>Last 7 days</option>
+                    <select name="" id="" style={{ padding: '10px', borderRadius: '10px', background: '#E8EDF1', borderColor: '#4BA8A8' }}>
+                        <option value="" style={{ color: '#738383' }}>Last 7 days</option>
                     </select>
                 </div>
 
--- src/components/lineChart.tsx/LineChart.tsx
+++ src/components/lineChart.tsx/LineChart.tsx
@@ -62,14 +62,14 @@ const data = {
             data: [33, 53, 85, 41, 44, 65],
             fill: true,
             // backgroundColor: "rgba(75,192,192,0.2)",
-            backgroundColor: "#742774",
+            backgroundColor: "#F8B400",
             borderColor: "rgba(75,192,192,1)"
         },
         {
             label: "Second dataset",
             data: [33, 25, 35, 51, 54, 76],
             fill: false,
-            borderColor: "#742774"
+            borderColor: "#F8B400"
         }
     ]
 };
